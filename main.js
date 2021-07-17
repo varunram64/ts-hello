a41833fce5bf5d7938d2b7b3ab257cb6be4499c7
Varun Kalidindi <varunram64@gmail.com>
2021-07-17 20:40:14 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function log(msg) {
+    console.log(msg);
+}
+var message = 'Hello World';
+log(message);
---
+++ main.ts
@@ -0,0 +1,10 @@
+function log(msg) {
+    console.log(msg);
+}
+
+var message = 'Hello World';
+
+log(message);
+
+var number = 1;
+let count = 2;
