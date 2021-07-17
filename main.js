b3fbcc2f9f610ddba64f829c4b0d49628da37c01
Varun Kalidindi <varunram64@gmail.com>
2021-07-17 20:48:22 +0530

    Difference between let and var

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ function log(msg) {
 }
 var message = 'Hello World';
 log(message);
+var number = 1;
+function doSomething(i) {
+    for (i = 0; i < 5; i++) {
+        log(i);
+        //let count = 2;// This can't be accessed outside the for loop
+    }
+    log('finally:' + i);
+}
+doSomething(number);
--- main.ts
+++ main.ts
@@ -7,4 +7,14 @@ var message = 'Hello World';
 log(message);
 
 var number = 1;
-let count = 2;
+
+function doSomething(i) {
+    for(i =0; i < 5; i++) {
+        log(i);
+        //let count = 2;// This can't be accessed outside the for loop
+    }
+
+    log('finally:' + i);
+}
+
+doSomething(number);
