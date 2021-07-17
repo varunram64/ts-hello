fe6140528d1f14818fe5af2b7f64de1f32e0f6b6
Varun Kalidindi <varunram64@gmail.com>
2021-07-17 22:27:19 +0530

    Classes

---
+++ next.ts
@@ -0,0 +1,22 @@
+class Point {
+    x: number;
+    y: number;
+    draw() {
+        //....
+    }
+
+    getDistance(another: Point) {
+        //....
+    }
+}
+
+// let drawPoint = (point: Point) => {
+//     //...   
+//     console.log('x:' + point.x);
+//     console.log('y:' + point.y); 
+// }
+
+// drawPoint({
+//     x: 1,
+//     y: 2
+// });
