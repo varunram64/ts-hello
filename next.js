3d89ae274b2d23c6360722e9760d023ec9df27c8
Varun Kalidindi <varunram64@gmail.com>
2021-07-18 08:44:16 +0530

    Constructors

--- next.js
+++ next.js
@@ -1,18 +1,20 @@
 var Point = /** @class */ (function () {
-    function Point() {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
     }
     Point.prototype.draw = function () {
         console.log('x:' + this.x);
         console.log('y:' + this.y);
     };
-    Point.prototype.getDistance = function (another) {
-        //....
-    };
     return Point;
 }());
-var point = new Point();
-point.x = 1;
-point.y = 2;
+var point = new Point(1, 2);
+point.draw();
+// Default constructor an supply each parameter
+point = new Point(); // object for an instance of Point class
+// point.x = 1; 
+// point.y = 2;
 point.draw();
 // let drawPoint = (point: Point) => {
 //     //...   
--- next.ts
+++ next.ts
@@ -1,19 +1,25 @@
 class Point {
     x: number;
     y: number;
+
+    constructor(x?: number, y?: number) {
+        this.x = x;
+        this.y = y;
+    }
+
     draw() {
         console.log('x:' + this.x);
         console.log('y:' + this.y); 
     }
-
-    getDistance(another: Point) {
-        //....
-    }
 }
 
-let point = new Point(); // object for an instance of Point class
-point.x = 1;
-point.y = 2;
+let point = new Point(1, 2);
+point.draw();
+
+// Default constructor an supply each parameter
+ point = new Point(); // object for an instance of Point class
+// point.x = 1; 
+// point.y = 2;
 point.draw();
 
 // let drawPoint = (point: Point) => {
