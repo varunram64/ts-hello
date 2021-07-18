d515aa21f89e188785019f0dfcf42c0262e69883
Varun Kalidindi <varunram64@gmail.com>
2021-07-18 10:16:04 +0530

    Properties

--- next.js
+++ next.js
@@ -11,15 +11,28 @@ var Point = /** @class */ (function () {
         console.log('x:' + this.x);
         console.log('y:' + this.y);
     };
+    Object.defineProperty(Point.prototype, "X", {
+        get: function () {
+            return this.x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error('value cannot be less than 0');
+            this.x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Point;
 }());
 var point = new Point(1, 2);
+point.X = 5;
 point.draw();
 // Default constructor an supply each parameter
-point = new Point(); // object for an instance of Point class
+// point = new Point(); // object for an instance of Point class
 // point.x = 1; 
 // point.y = 2;
-point.draw();
+// point.draw();
 // let drawPoint = (point: Point) => {
 //     //...   
 //     console.log('x:' + point.x);
--- next.ts
+++ next.ts
@@ -2,25 +2,40 @@ class Point {
     // private x: number;
     // private y: number;
 
-    constructor(private x?: number, private y?: number) {
+    constructor(private _x?: number, private _y?: number) {
         // this.x = x;
         // this.y = y;
     }
 
     draw() {
-        console.log('x:' + this.x);
-        console.log('y:' + this.y); 
+        console.log('x:' + this._x);
+        console.log('y:' + this._y); 
+    }
+
+    // Getter
+    get x() {
+        return this._x;
+    }
+
+    // Setter
+    // Note: If this is commented or not present then the property is ready only
+    set x(value) {
+        if(value < 0)
+            throw new Error('value cannot be less than 0');
+        this._x = value;
     }
 }
 
 let point = new Point(1, 2);
+point.x = 5;
+
 point.draw();
 
 // Default constructor an supply each parameter
- point = new Point(); // object for an instance of Point class
+// point = new Point(); // object for an instance of Point class
 // point.x = 1; 
 // point.y = 2;
-point.draw();
+// point.draw();
 
 // let drawPoint = (point: Point) => {
 //     //...   
