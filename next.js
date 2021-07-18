78d340f469be81ce89097d416a6c58021874715a
Varun Kalidindi <varunram64@gmail.com>
2021-07-18 08:37:42 +0530

    Objects

--- next.js
+++ next.js
@@ -1,9 +1,25 @@
-var drawPoint = function (point) {
-    //...   
-    console.log('x:' + point.x);
-    console.log('y:' + point.y);
-};
-drawPoint({
-    x: 1,
-    y: 2
-});
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log('x:' + this.x);
+        console.log('y:' + this.y);
+    };
+    Point.prototype.getDistance = function (another) {
+        //....
+    };
+    return Point;
+}());
+var point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
+// let drawPoint = (point: Point) => {
+//     //...   
+//     console.log('x:' + point.x);
+//     console.log('y:' + point.y); 
+// }
+// drawPoint({
+//     x: 1,
+//     y: 2
+// });
--- next.ts
+++ next.ts
@@ -2,13 +2,19 @@ class Point {
     x: number;
     y: number;
     draw() {
-        //....
+        console.log('x:' + this.x);
+        console.log('y:' + this.y); 
     }
 
     getDistance(another: Point) {
         //....
     }
 }
+
+let point = new Point(); // object for an instance of Point class
+point.x = 1;
+point.y = 2;
+point.draw();
 
 // let drawPoint = (point: Point) => {
 //     //...   
